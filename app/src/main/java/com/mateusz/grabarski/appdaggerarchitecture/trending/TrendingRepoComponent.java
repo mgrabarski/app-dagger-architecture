7f8873e2d2cce7d9576929a8ff1af6b383ca1d71
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-02-20 18:40:04 +0100

    Fix for wrong extended injector

--- app/src/main/java/com/mateusz/grabarski/appdaggerarchitecture/trending/TrendingRepoComponent.java
+++ app/src/main/java/com/mateusz/grabarski/appdaggerarchitecture/trending/TrendingRepoComponent.java
@@ -1,5 +1,7 @@
 package com.mateusz.grabarski.appdaggerarchitecture.trending;
 
+import com.mateusz.grabarski.appdaggerarchitecture.di.scopes.ScreenScope;
+
 import dagger.Component;
 import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
@@ -7,9 +9,9 @@ import dagger.android.AndroidInjector;
 /**
  * Created by Mateusz Grabarski on 12.02.2018.
  */
-@Component
+@ScreenScope
 @Subcomponent
-public interface TrendingRepoComponent extends AndroidInjector<TrendingRepoComponent> {
+public interface TrendingRepoComponent extends AndroidInjector<TrendingReposController> {
 
     @Subcomponent.Builder
     abstract class Builder extends AndroidInjector.Builder<TrendingReposController> {
